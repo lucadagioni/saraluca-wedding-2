6c4fa2429ae9c37588d9dc826f10c0d6cb3ae565
Luca Dagioni <luca.dagioni@gmail.com>
2025-07-05 22:39:28 +0200

    Fix Rollup build error in GitHub Actions - use clean install and add rollup config

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,9 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [],
   base: '/saraluca-wedding-2/',
+  build: {
+    rollupOptions: {
+      external: [],
+    },
+  },
 })
